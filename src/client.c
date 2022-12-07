1ab97a4874d3f0e21e7189834a89edded2ca64e5
Toyin Balogun <balogunt302@gmail.com>
2022-12-06 19:13:34 -0500

    pushing Demi's changes to server + client source files

--- src/client.c
+++ src/client.c
@@ -6,41 +6,64 @@
 #include <sys/dispatch.h>
 
 #define MAX_STRING_LEN    256
-
+#define TIMER_PULSE_EVENT (_PULSE_CODE_MINAVAIL + 7)
 //resources:
-
+//http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/c/connectattach.html
 //http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/m/msgsend.html
-//http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/n/name_open.html
-//http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/n/name_close.html
 
 
 int main(int argc, char **argv)
 {
-	//Declare variables needed
-	int rcvid, coid, reply;
-	char msg[MAX_STRING_LEN +1];
-	char *server_name = "server";
-	
-	//set the message value to 'test'
-	strcpy(msg, "test");
-	//establish a connection to the server's channel
-	
-	coid = name_open(server_name, 0);
+    int coid, status;
+    char message[MAX_STRING_LEN] = "test";
+    int incoming_msg, pid;
+    timer_t timerid;
+    struct itimerspec it;
+    struct sigevent event;
 
-	//send the message to the server and get a reply
-	rcvid = MsgSend(coid, msg, sizeof(msg), &reply, sizeof(reply));
+     typedef struct
+    {
+        int load;// [0,1,2]  0 = low , 1 = mid = 2 = high
+        int state; // 0 if not working , 1 if its active
+    } client_t;
 
-    //print received checksum
-	printf("Received checksum: %d\n", reply);
+    client_t client_status;
 
-    //print returned status
-	printf("Returned Status: %d\n", rcvid);
+    //Create a Connection to Server Channel
+    coid = name_open("Server Room",0);
 
-    //send a pulse to the server
-	MsgSendPulse(coid, 0, 2, 100);
+    //Print client pid
+    pid = getpid();
+    printf("My Pid is %d \n", pid);
 
-    //Close a server connection that was opened by name_open()
-	name_close(coid);
 
-	return 0;
+    /* set up the pulse event that will be delivered to us by the kernel
+     * whenever the timer expires  (USE the MACRO SIGEV_PULSE_INIT)
+     */
+    SIGEV_PULSE_INIT(&event, coid, SIGEV_PULSE_PRIO_INHERIT, TIMER_PULSE_EVENT, &timerid);
+
+    /* Create a timer which will send the above pulse event
+     * 5 seconds from now and then repeatedly after that every
+     * 1500 milliseconds(1.5 secs).  The event to use has already been filled in
+     * above and is in the variable called 'event'.
+     */
+    timer_create(CLOCK_REALTIME, &event, &timerid);
+    it.it_value.tv_sec = 5;
+    it.it_value.tv_nsec = 0;
+    it.it_interval.tv_sec = 1;
+    it.it_interval.tv_nsec = 500000000;
+    timer_settime(timerid, 0, &it, NULL);
+
+
+
+
+
+    //send the message to the server and get a reply
+    status = MsgSend(coid, &message, sizeof(message), &incoming_msg, sizeof(incoming_msg));
+//    printf("message is %s \n", message);
+//    printf("Status is %d \n", status);
+
+
+
+    return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,95 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/neutrino.h>
+#include <process.h>
+#include <errno.h>
+#include <sys/netmgr.h>
+#include <string.h>
 #include <sys/iofunc.h>
 #include <sys/dispatch.h>
 
-
 //resources:
-
+//http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/c/channelcreate.html
 //http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/m/msgreceive.html
 //http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/m/msgreply.html
-//http://www.qnx.com/developers/docs/7.1/#com.qnx.doc.neutrino.lib_ref/topic/n/name_attach.html
 #define MAX_STRING_LEN    256
+#define ARR_SIZE 10
 
-
-int calculate_checksum(char *text);
+void print_pid(int arr[]);
 
 int main(void) {
+    int chid, pid, rcvid, myMsgReply;
+    name_attach_t *attach;
+    struct _msg_info info;
+    int checksumMsg =0;
 
-	typedef union
-	{
-		struct _pulse pulse;
-	    char rmsg [MAX_STRING_LEN +1];
-	} myMessage_t;
+    typedef union
+    {
+        struct _pulse pulse;
+        char rmsg [MAX_STRING_LEN +1];
+    } myMessage_t;
 
+     typedef struct
+    {
+        int load;// [0,1,2]  0 = low , 1 = mid = 2 = high
+        int state; // 0 if not working , 1 if its active
+    } server_t;
 
-	
-	//attach a name to the channel
-	name_attach_t *attach = name_attach(NULL, "server", 0);
-	int chid = attach->chid;
-	
-	//create a message
-	myMessage_t msg;
+    myMessage_t msg;
 
-    //the server should keep receiving, processing and replying to messages
-	while(1)
-	{
-		//code to receive msg or pulse from client
-		int rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
+    //Create a Channel
+    attach = name_attach(NULL, "Server Room", 0);
 
-		//print the returned value of MsgReceive
-		printf("MsgReceive returned %d\n", rcvid);
+    while(1){
+        rcvid = MsgReceive(attach->chid, &msg.rmsg, sizeof(msg.rmsg), &info);
 
-		//check if it was a pulse or a message
-		// if it is a pulse -> check the pulse code to see if the client is gone/disconnected and print (client is gone)  else if the pulse is something else print the code and value of the pulse
+        //Print Rcvid
+        printf("Rcvid: %d \n", rcvid);
+        printf("Client pid is %d \n", info.pid);
+        //print_pid(pidArray);
+        myMsgReply = MsgReply(rcvid, EOK, &checksumMsg, sizeof(checksumMsg));
+    }
 
-	  if (rcvid == 0)
-	  {
-		  //check pulse type
-		  if (msg.pulse.code == _PULSE_CODE_DISCONNECT)
-		  {
-			 
-			  printf("Client is gone\n");
-			  
-	
-		  }
-		  printf("Pulse received: %d\n", msg.pulse.code);
-	  }
+//    }
 
-		// if it was a message
-	  else
-	  {
-		  //print received message
-		  printf("Message received: %s\n", msg.rmsg);
-		  
-		  //calculate checksum by calling calculate_checksum
-		  int checksum = calculate_checksum(msg.rmsg);
-
-		  //reply to client with checksum
-		  MsgReply(rcvid, 0, &checksum, sizeof(checksum));
-
-		  //print the return value of MsgReply
-		  printf("MsgReply returned: %d\n", rcvid);
-	  }
-
-	}
-		//remove the name from the namespace and destroy the channel
-		name_detach(attach, 0);
-		ChannelDestroy(chid);
-
-return 0;
-}
-
-
-
-
-int calculate_checksum(char *text)
-{
-	char *c;
-	int cksum = 0;
-
-	for (c = text; *c != '\0'; c++)
-		cksum += *c;
-	return cksum;
+    return 0;
 }
